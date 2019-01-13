348ac80bd106a73b95fbbd6b05c099fe812960d6
88Sasha88 <46623683+88Sasha88@users.noreply.github.com>
2019-01-12 18:13:36 -0800

    Update Transpose.f90

--- Transpose.f90
+++ Transpose.f90
@@ -19,7 +19,7 @@ program Transpose
     do
         read(3, *, iostat = IO) dummyVariable
         if (IO > 0) then ! I think this means you're reading past the total elements in the file.
-            print*, "Your shit's fucked up."
+            print*, "Bad news."
             exit
         else if (IO < 0) then ! I think this means you're reading past the total rows in the file.
             exit
@@ -41,7 +41,7 @@ program Transpose
     do
         read(3, *, iostat = IO) fillerArray
         if (IO > 0) then
-            print*, "Your shit's fucked up."
+            print*, "Bad news."
             exit
         else if (IO < 0) then
             exit
